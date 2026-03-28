6f5ccbb7e3f80347033eaa6971842e52fe740583
neomunda <suskactfour@gmail.com>
2026-03-28 17:18:11 -0400

    oh the misery

--- kubejs/startup_scripts/main.js
+++ kubejs/startup_scripts/main.js
@@ -1,3 +1,5 @@
-// Visit the wiki for more info - https://kubejs.com/
-console.info('Hello, World! (Loaded startup example script)')
 
+StartupEvents.registry('item', event => {
+  event.create('heated_gold_ingot').tag('overgeared:heated_metals')
+  event.create('heated_zinc_ingot').tag('overgeared:heated_metals')
+})
