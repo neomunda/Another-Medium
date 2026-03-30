3a665377828a45317c0a979887734bbb8fd1961f
neomunda <suskactfour@gmail.com>
2026-03-30 19:18:32 -0400

    forgot to push this yesterday lol

--- kubejs/startup_scripts/main.js
+++
@@ -1,5 +0,0 @@
-
-StartupEvents.registry('item', event => {
-  event.create('heated_gold_ingot').tag('overgeared:heated_metals')
-  event.create('heated_zinc_ingot').tag('overgeared:heated_metals')
-})
