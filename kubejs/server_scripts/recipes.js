241f8563e6d65f039ad35d1454d985778f104452
neomunda <suskactfour@gmail.com>
2026-08-06 17:15:38 -0400

    0.1c progress

--- kubejs/server_scripts/recipes.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.recipes(event => {
-event.remove({ type: 'minecraft:blasting' })
-event.remove({ type: 'minecraft:smoking' })
-})
--- kubejs/server_scripts/tags.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.tags('block', event => {
-  event.remove('minecraft:incorrect_for_wooden_tool', '/^minecraft:.*copper.*/')
-  event.remove('minecraft:needs_stone_tool', '/^minecraft:.*copper.*/')
-  event.remove('minecraft:incorrect_for_gold_tool', '/^minecraft:.*copper.*/')
-
-  event.add('minecraft:needs_iron_tool', '/^*deepslate.*/')
-})
